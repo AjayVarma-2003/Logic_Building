e0fa371449aabb54f1b7af79a0c586ebb3aa83fa
AjayVarma-2003 <ajayvarma2003@gmail.com>
2023-11-18 12:52:27 +0530

    Initial Commit

---
+++ Program134.c
@@ -0,0 +1,31 @@
+# include <stdio.h>
+
+int CountVowel(char str[])
+{
+    int iCnt = 0;
+
+    while(*str != '\0')
+    {
+        if(*str == 'A' || *str == 'E' || *str == 'I' || *str == 'O' || *str == 'U')
+        {
+            iCnt ++;
+        }
+        str ++;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    char Arr[20];
+    int iRet = 0;
+
+    printf("Enter the string : \n");
+    scanf("%[^'\n]s",Arr);
+
+    iRet = CountVowel(Arr);
+
+    printf("Frequency of the vowels in the string : %d \n",iRet);   
+
+    return 0;
+}
