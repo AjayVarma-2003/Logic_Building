ac57dbd76a06c1e3275604bb1a686ab054f6adfd
AjayVarma-2003 <ajayvarma2003@gmail.com>
2024-01-08 18:46:57 +0530

    Initial Commit

--- Program216.cpp
+++ Program216.cpp
@@ -1,41 +1,11 @@
-#include<iostream>
-using namespace std;
-
-typedef struct node
-{
-    int data;
-    struct node *next;
-}NODE, *PNODE, **PPNODE;
-
-class SinglyLL
-{
-    public:
-        PNODE first;
-        int Count;
-
-        SinglyLL()
-        {
-            first = NULL;
-            Count = 0;
-        }
-
-        void InsertFirst(int no);
-        void InsertLast(int no);
-        void InsertAtPos(int no, int ipos);
-        void DeleteFirst();
-        void DeleteLast();
-        void DeleteAtPos(int ipos);
-        void Display();
-};
-
-int main()
-{
-    SinglyLL obj();
-
-    obj.InsertFirst(51);
-    obj.InsertFirst(21);
-    obj.InsertFirst(11);
-    
-    obj.Display();
-    return 0;
-}
+Demat ID
+(For IPO application and CDSL TPIN authorisation)
+1208160080253751
+DP ID
+12081600
+BO ID
+80253751
+Depository Participant (DP)
+Zerodha Broking Limited
+Depository
+CDSL
