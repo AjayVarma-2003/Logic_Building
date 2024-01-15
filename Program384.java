9dd145aff8e279536b0eaa2b9f41526eab229e38
AjayVarma-2003 <ajayvarma2003@gmail.com>
2024-01-15 10:37:45 +0530

    Initial Commit

---
+++ Program384.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Program384
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iROw = 0, iCol = 0;
+
+        System.out.println("Enter the number of rows : ");
+        iROw = sobj.nextInt();
+
+        System.out.println("Enter the number of colomns : ");
+        iCol = sobj.nextInt();
+
+        int Arr[][] = new int[iROw][iCol];
+    }
+}
